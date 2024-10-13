81232d9f9be11ac3794cf40a3da33c3d1ba2bb06
AndyJrII <enehizenajames@gmail.com>
2024-10-13 02:53:32 +0100

    built header, sections

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Function to detect if the element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Scroll event listener
+window.addEventListener('scroll', function () {
+  const image = document.querySelector('.slide-in-image');
+  if (isInViewport(image)) {
+    image.classList.add('show');
+  }
+});
